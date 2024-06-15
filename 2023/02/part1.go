0fc0daa8d87c40f15da15d07f3215455b509e59e
Andrew Tang <actang150@gmail.com>
2024-06-15 10:05:51 -0700

    True implementation of 2023/02 part 1

--- 2023/02/part1.go
+++ 2023/02/part1.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func part1(input string) int {
 	res := 0
 	for i := 0; i < len(input); i++ {
@@ -19,21 +24,33 @@ func part1(input string) int {
 	return res
 }
 
-func part1line(line string) (game int, valid bool) {
-	if line == "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" {
-		return 1, true
+const (
+	rlimit = 12
+	glimit = 13
+	blimit = 14
+)
+
+func part1line(line string) (int, bool) {
+	before, after, _ := strings.Cut(line[5:], ": ")
+	game, _ := strconv.Atoi(before)
+
+	for _, set := range strings.Split(after, "; ") {
+		for _, marble := range strings.Split(set, ", ") {
+			before, after, _ := strings.Cut(marble, " ")
+			count, _ := strconv.Atoi(before)
+			r, g, b := 0, 0, 0
+			switch after {
+			case "red":
+				r = count
+			case "green":
+				g = count
+			case "blue":
+				b = count
+			}
+			if r > rlimit || g > glimit || b > blimit {
+				return game, false
+			}
+		}
 	}
-	if line == "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue" {
-		return 2, true
-	}
-	if line == "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red" {
-		return 3, false
-	}
-	if line == "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red" {
-		return 4, false
-	}
-	if line == "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" {
-		return 5, true
-	}
-	return
+	return game, true
 }
