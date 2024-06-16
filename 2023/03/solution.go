141ed035ec7ce376d8e5ea9c674c0b70b27f72cb
Andrew Tang <actang150@gmail.com>
2024-06-16 14:08:08 -0700

    Implement 2023/03 part 2 and clean up part 1

--- 2023/03/solution.go
+++ 2023/03/solution.go
@@ -11,6 +11,7 @@ var input string
 
 func main() {
 	fmt.Println(part1(input))
+	fmt.Println(part2(input))
 }
 
 func part1(input string) int {
@@ -19,8 +20,8 @@ func part1(input string) int {
 	res := 0
 	for j, row := range matrix {
 		part, isPart := 0, false
-		for i, c := range row {
-			if c >= '0' && c <= '9' {
+		for i := 0; i < len(row); i++ {
+			if c := row[i]; isDigit(c) {
 				part = part*10 + int(c-'0')
 				isPart = isPart || hasSymbol(matrix, j, i)
 				continue
@@ -66,6 +67,128 @@ func hasSymbol(matrix []string, j, i int) bool {
 	return false
 }
 
+func isDigit(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
 func isSymbol(c byte) bool {
-	return c != '.' && (c < '0' || c > '9')
+	return c != '.' && !isDigit(c)
+}
+
+const gear = '*'
+
+func part2(input string) int {
+	matrix := strings.Split(input, "\n")
+
+	res := 0
+	for j, row := range matrix {
+		for i, c := range row {
+			if c == gear {
+				gearRatio, isGear := inspectSurrounding(matrix, j, i)
+				if isGear {
+					res += gearRatio
+				}
+			}
+		}
+	}
+	return res
+}
+
+func inspectSurrounding(matrix []string, j, i int) (gearRatio int, isGear bool) {
+	count, gearRatio := 0, 1
+
+	left := i - 1
+	safeLeft := left > -1
+	if safeLeft && isDigit(matrix[j][left]) {
+		count++
+		partNum := getPartNum(matrix[j], left)
+		gearRatio *= partNum
+	}
+	right := i + 1
+	safeRight := right < len(matrix[j])
+	if safeRight && isDigit(matrix[j][right]) {
+		count++
+		partNum := getPartNum(matrix[j], right)
+		gearRatio *= partNum
+		if count == 2 {
+			isGear = true
+			return
+		}
+	}
+
+	if up := j - 1; up > -1 {
+		if isDigit(matrix[up][i]) {
+			count++
+			partNum := getPartNum(matrix[up], i)
+			gearRatio *= partNum
+			if count == 2 {
+				isGear = true
+				return
+			}
+		} else {
+			if safeLeft && isDigit(matrix[up][left]) {
+				count++
+				partNum := getPartNum(matrix[up], left)
+				gearRatio *= partNum
+				if count == 2 {
+					isGear = true
+					return
+				}
+			}
+			if safeRight && isDigit(matrix[up][right]) {
+				count++
+				partNum := getPartNum(matrix[up], right)
+				gearRatio *= partNum
+				if count == 2 {
+					isGear = true
+					return
+				}
+			}
+		}
+	}
+
+	if down := j + 1; down > -1 {
+		if isDigit(matrix[down][i]) {
+			count++
+			partNum := getPartNum(matrix[down], i)
+			gearRatio *= partNum
+			if count == 2 {
+				isGear = true
+				return
+			}
+		} else {
+			if safeLeft && isDigit(matrix[down][left]) {
+				count++
+				partNum := getPartNum(matrix[down], left)
+				gearRatio *= partNum
+				if count == 2 {
+					isGear = true
+					return
+				}
+			}
+			if safeRight && isDigit(matrix[down][right]) {
+				count++
+				partNum := getPartNum(matrix[down], right)
+				gearRatio *= partNum
+				if count == 2 {
+					isGear = true
+					return
+				}
+			}
+		}
+	}
+	return
+}
+
+func getPartNum(row string, i int) int {
+	for i > -1 && isDigit(row[i]) {
+		i--
+	}
+	i++
+	res := 0
+	for i < len(row) && isDigit(row[i]) {
+		res = res*10 + int(row[i]-'0')
+		i++
+	}
+	return res
 }
--- 2023/03/solution_test.go
+++ 2023/03/solution_test.go
@@ -27,3 +27,18 @@ func BenchmarkPart1(b *testing.B) {
 		part1(input1)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	want := 467835
+	got := part2(input1)
+
+	if want != got {
+		t.Fatalf("Invalid result for input \n%s. got %d, want %d", input1, got, want)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part2(input1)
+	}
+}
