4824dd607bffc808ad17d4bf4e2997764f838280
Andrew Tang <actang150@gmail.com>
2024-06-25 16:34:25 -0700

    Code gen 2023 day 7

---
+++ 2023/07/solution_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+const input1 = ``
+
+func TestPart1(t *testing.T) {
+	want := 0
+	got := part1(input1)
+
+	if got != want {
+		t.Fatalf("Invalid result for input %s. got %d, want %d", input1, got, want)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part1(input1)
+	}
+}
+
+const input2 = ``
+
+func TestPart2(t *testing.T) {
+	want := 0
+	got := part2(input2)
+
+	if got != want {
+		t.Fatalf("Invalid result for input %s. got %d, want %d", input2, got, want)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part2(input2)
+	}
+}
